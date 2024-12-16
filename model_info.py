7d7a3042ea3193d7eb103e6f1126a739a22defa8
EC Computer <dontwantno@association.noway>
2024-12-16 11:32:28 -0600

    cleaned up model_info.py

--- model_info.py
+++ model_info.py
@@ -1,4 +1,10 @@
 import spacy
 
 models = ['tagalog/output/tl_dep_news_sm', 'persian/output/fa_dep_web_sm', 
-          'indonesian/output/id_news_sm', 'arabic/output/ar_web_sm']
+          'indonesian/output/id_dep_web_sm', 'arabic/output/ar_dep_web_sm']
+
+for model in models:
+    nlp = spacy.load(model)
+    # Get the size of the word vector table
+    
+    #### test here ####
